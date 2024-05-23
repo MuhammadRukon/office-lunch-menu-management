e0262a11fda0f537d3f560c9394efe78fb9cff02
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-05-23 18:44:50 +0600

    feat: home page complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,41 @@
 import MainLayout from "../../layout/MainLayout";
+import Card from "./../../component/home/Card.jsx"
+
+
 
 const Home = () => {
+  const role = "admin"
+  const cardsData = [
+    {
+      name: "Add Daily Menu Options",
+      for: "admin",
+      path: "/add-menu"
+    },
+    {
+      name: "View Employee Choices",
+      for: "admin",
+      path: "/view-choice"
+    },
+    {
+      name: "View Daily Menu",
+      for: "employee",
+      path: "/view-menu"
+    },
+    {
+      name: "Select Lunch Choice",
+      for: "employee",
+      path: "/select-lunch"
+    },
+  ];
   return (
     <>
-      <MainLayout>Homepage</MainLayout>
+      <MainLayout>
+        <div className="flex gap-[10%] lg:gap-[20%] justify-center items-center h-[calc(100vh-200px)]">
+          {cardsData.map((data, idx) => (
+            data.for === role && <Card data={data} key={idx} />
+          ))}
+        </div>
+      </MainLayout>
     </>
   );
 };
